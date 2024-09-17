65866f7f7764797ece5690be66f1cacd66a75dfc
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-09-17 13:23:52 +0200

    adjustment controller

--- src/main/kotlin/com/example/karnor/controller/PdfController.kt
+++ src/main/kotlin/com/example/karnor/controller/PdfController.kt
@@ -1,10 +1,14 @@
 package com.example.karnor.controller
 
+import com.example.karnor.model.Pdf
+import com.example.karnor.repository.PdfRepo
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class PdfController {
+class PdfController(val pdfRepo: PdfRepo) { //Dependency injection
 
     @GetMapping("/test")
     fun test(): String {
@@ -13,5 +17,11 @@ class PdfController {
     @GetMapping("")
     fun test2(): String {
         return "Viva la Röva"
+    }
+
+    @PostMapping("/save")
+    fun savePdf(@RequestBody pdf: Pdf): String   {
+        pdfRepo.save(pdf)
+        return "PDF:en sparades ner!"
     }
 }
--- src/main/kotlin/com/example/karnor/repository/PdfRepo.kt
+++ src/main/kotlin/com/example/karnor/repository/PdfRepo.kt
@@ -2,6 +2,8 @@ package com.example.karnor.repository
 
 import com.example.karnor.model.Pdf
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface PdfRepo : JpaRepository<Pdf, Long> {
 }
