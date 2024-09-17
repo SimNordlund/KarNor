084c0572b4884409f7d707cc554ceffd1ede6de1
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-09-17 15:56:04 +0200

    added DTO

---
+++ src/main/kotlin/com/example/karnor/dto/PdfDTO.kt
@@ -0,0 +1,11 @@
+package com.example.karnor.dto
+
+import java.time.LocalDateTime
+
+//Validera med @field?
+data class PdfDTO(
+    val id: Long? = null,
+    val fileName: String?,
+    val data: ByteArray?,
+    val uploadTime: LocalDateTime? 
+)
