6ec1144966ea5e018c4761c2934f4b570d0fe1de
SimNordlund <147151891+SimNordlund@users.noreply.github.com>
2024-09-22 22:03:00 +0200

    bugfix with seeder

--- src/main/kotlin/com/example/karnor/repository/PdfRepo.kt
+++ src/main/kotlin/com/example/karnor/repository/PdfRepo.kt
@@ -3,8 +3,10 @@ package com.example.karnor.repository
 import com.example.karnor.model.Pdf
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
 interface PdfRepo : JpaRepository<Pdf, Long> {
-    fun findByFileName(fileName: String): Pdf?
+    @Transactional(readOnly = true)
+    fun findByFileName(name : String): Pdf
 }
